e8f96901859860fb62d5318428e2832d67cca572
Pontus Alexander <pontus.alexander@gmail.com>
2020-05-09 20:19:19 +0200

    Update BF interpreter

--- bf/bf.js
+++ bf/bf.js
@@ -5,85 +5,63 @@ function readScript(path) {
     return fs.readFileSync(path);
 }
 
-function debug(...args) {
-    console.debug(...args);
-}
-
 function print(value) {
     const string = String.fromCharCode(value);
     process.stdout.write(string);
 }
 
-const scriptArg = process.argv[2];
-const script = readScript(scriptArg);
+function interpret(chars) {
+    const cells = new Uint8Array(30000).fill(0);
+    let ptr = 0;
 
-const cells = new Array(30).fill(0);
-let ptr = 0;
-
-const chars = script.toString('ascii');
-const loopIndices = [];
-for (let i = 0; i < chars.length; i++) {
-    debug('Char: %d', i);
-    const char = chars[i];
-    if (char === "+") {
-        cells[ptr]++;
-        debug("Ptr [%d]: %d", ptr, cells[ptr]);
-    } else if (char === "-") {
-        cells[ptr]--;
-        debug("Ptr [%d]: %d", ptr, cells[ptr]);
-    } else if (char === ">") {
-        ptr++;
-        debug('Pointer: %d', ptr);
-        if (ptr > cells.length - 1) {
-            const context = chars.slice(i, i + 100);
-            throw new RangeError(`Pointer above max @ ${i}: ${context}`);
-        }
-    } else if (char === "<") {
-        ptr--;
-        debug('Pointer: %d', ptr);
-        if (ptr < 0) {
-            const context = chars.slice(i, i + 100);
-            throw new RangeError(`Pointer below 0 @ ${i}: ${context}`);
-        }
-    } else if (char === "[") {
-        if (cells[ptr] === 0) {
-            for (let depth = 0;; i++) {
-                const char = chars[i];
-                if (char === "[") {
-                    depth++;
-                    debug("Increasing depth to %d", depth);
-                }
-                else if (char === "]") {
-                    depth--;
-                    debug("Decreasing depth to %d", depth);
-
-                    if (depth === 0) {
-                        debug("Found matching brace @ %d", i);
-                        break;
+    for (let i = 0; i < chars.length; i++) {
+        const char = chars[i];
+        if (char === "+") {
+            cells[ptr]++;
+        } else if (char === "-") {
+            cells[ptr]--;
+        } else if (char === ">") {
+            ptr++;
+        } else if (char === "<") {
+            ptr--;
+        } else if (char === "[") {
+            if (cells[ptr] === 0) {
+                for (let depth = 0;; i++) {
+                    const char = chars[i];
+                    if (char === "[") {
+                        depth++;
+                    }
+                    else if (char === "]") {
+                        depth--;
+                        if (depth === 0) {
+                            break;
+                        }
                     }
                 }
             }
-        }
-    } else if (char === "]") {
-        if (cells[ptr] > 0) {
-            for (let depth = 0;; i--) {
-                const char = chars[i];
-                if (char === "]") {
-                    depth++;
-                    debug("Increasing depth to %d", depth);
-                }
-                else if (char === "[") {
-                    depth--;
-                    debug("Decreasing depth to %d", depth);
-
-                    if (depth === 0) {
-                        debug("Found matching brace @ %d", i);
-                        break;
+        } else if (char === "]") {
+            if (cells[ptr] > 0) {
+                for (let depth = 0;; i--) {
+                    const char = chars[i];
+                    if (char === "]") {
+                        depth++;
+                    }
+                    else if (char === "[") {
+                        depth--;
+                        if (depth === 0) {
+                            break;
+                        }
                     }
                 }
             }
+        } else if (char === ".") {
+            print(cells[ptr]);
+        } else if (char === ",") {
+
         }
-    } else if (char === ".") {
-        print(cells[ptr]);
     }
 }
+
+const scriptArg = process.argv[2];
+const script = readScript(scriptArg);
+interpret(script.toString('ascii'));
